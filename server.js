232344c86a871c75fe8de252624bc37ec57b9e86
Alex Happe <abhdiver1994@gmail.com>
2019-12-18 20:47:23 -0600

    added a lot of stuff

--- server.js
+++ server.js
@@ -18,12 +18,25 @@ app.get("/add", function(req, res){
 app.get("/tables", function(req, res){
     res.sendFile(path.join(__dirname, "tables.html"))
 })
-app.get("/api/available", function(req, res){
+app.get("/api/tables", function(req, res){
     return res.json(available)
 })
-app.post("/api/available", function(req, res){
+app.get("/api/waiting", function(req, res){
+    return res.json(waiting)
+})
+app.post("/api/tables", function(req, res){
     var newTable = req.body
-    console.log(newTable)
+    console.log(available.length)
+    if(available.length <= 4){
+        available.push(newTable)
+        console.log(available)
+    }
+    else{
+        waiting.push(newTable)
+        console.log(waiting)
+    }
+    res.json(newTable)
+
 })
 
 app.listen(PORT, function(){
